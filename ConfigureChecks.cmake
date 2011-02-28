c3809fce15f966296c57961f0f4cf508b9b06dd4
samelian <samelian@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2011-02-28 22:51:39 +0000

    [kdegraphics/kview] initial cmake support
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/trinity/kdegraphics@1223181 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ ConfigureChecks.cmake
@@ -0,0 +1,13 @@
+#################################################
+#
+#  (C) 2010 Serghei Amelian
+#  serghei (DOT) amelian (AT) gmail.com
+#
+#  Improvements and feedback are welcome
+#
+#################################################
+
+# required stuff
+find_package( Qt )
+find_package( TQt )
+find_package( TDE )
