6f99c72ee500c7b189ff7fb372ea17257e06e5a2
samelian <samelian@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2011-05-05 22:47:10 +0000

    [kdegraphics] updated cmake files
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/trinity/kdegraphics@1230549 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- config.h.cmake
+++ config.h.cmake
@@ -7,3 +7,8 @@
 #cmakedefine HAVE_SYS_DIR_H 1
 #cmakedefine HAVE_NDIR_H 1
 #cmakedefine HAVE_PAPER_H 1
+
+// ksvg
+#define STDC_HEADERS 1
+#cmakedefine LCMS_HEADER <@LCMS_HEADER@>
+#cmakedefine HAVE_FREETYPE_2_2_x 1
