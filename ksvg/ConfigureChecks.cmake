c0a0dd885111a3072c3a3bbf296e7012e66070cc
samelian <samelian@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2011-05-05 22:46:25 +0000

    [kdegraphics/ksvg] added cmake support (svgdisplay crashing)
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/trinity/kdegraphics@1230548 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ ksvg/ConfigureChecks.cmake
@@ -0,0 +1,49 @@
+#################################################
+#
+#  (C) 2010-2011 Serghei Amelian
+#  serghei (DOT) amelian (AT) gmail.com
+#
+#  Improvements and feedback are welcome
+#
+#  This file is released under GPL >= 2
+#
+#################################################
+
+# lcms
+check_include_file( lcms.h HAVE_LCMS_H )
+if( HAVE_LCMS_H )
+  set( LCMS_HEADER "lcms.h" CACHE INTERNAL "" FORCE )
+endif( )
+if( LCMS_HEADER )
+  check_library_exists( lcms VEC3init "" HAVE_LCMS )
+endif( )
+if( HAVE_LCMS )
+  set( LCMS_LIBRARY lcms )
+else( )
+  tde_message_fatal( "lcms is required, but was not found on your system" )
+endif( )
+
+
+# libart-2.0
+pkg_search_module( ART libart-2.0 )
+if( NOT ART_FOUND )
+  tde_message_fatal( "libart-2.0 is required, but was not found on your system" )
+endif( )
+
+
+# freetype2
+pkg_search_module( FREETYPE freetype2 )
+if( NOT FREETYPE_FOUND )
+  tde_message_fatal( "freetype2 is required, but was not found on your system" )
+endif( )
+if( NOT ${FREETYPE_VERSION} VERSION_LESS "2.2" )
+  set( HAVE_FREETYPE_2_2_x 1 CACHE INTERNAL "" FORCE )
+endif( )
+
+
+
+# fribidi
+pkg_search_module( FRIBIDI fribidi )
+if( NOT FRIBIDI_FOUND )
+  tde_message_fatal( "fribidi is required, but was not found on your system" )
+endif( )
