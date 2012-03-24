d7b34849bf17e073c2ad8b85587122967e3ae0c7
Darrell Anderson <humanreadable@yahoo.com>
2012-03-23 19:42:51 -0500

    Fix KDE -> TDE branding.

--- kolourpaint/kpmainwindow_help.cpp
+++ kolourpaint/kpmainwindow_help.cpp
@@ -138,7 +138,7 @@ void kpMainWindow::slotHelpTakingScreenshots ()
 
             "<p>"
             "You may configure the <b>Desktop Screenshot</b> shortcut"
-            " in the KDE Control Center"
+            " in the TDE Control Center"
             " module <a href=\"configure kde shortcuts\">Keyboard Shortcuts</a>."
             "</p>"
 
