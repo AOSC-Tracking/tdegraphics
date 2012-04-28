0efb8a18d45f2432c9c796aa5ef7af3f124967ea
Darrell Anderson <humanreadable@yahoo.com>
2012-04-27 23:24:31 -0500

    GCC 4.7 fix. This partially resolves bug report 958. Thanks to David C. Rankin and Serghei Amelian.

--- ksnapshot/ConfigureChecks.cmake
+++ ksnapshot/ConfigureChecks.cmake
@@ -9,4 +9,13 @@
 #
 #################################################
 
-check_include_file( X11/extensions/shape.h HAVE_X11_EXTENSIONS_SHAPE_H )
+## test for and explicitly include libXext
+if( WITH_XEXT )
+  pkg_search_module( XEXT xext )
+  if( XEXT_FOUND )
+    check_include_file( "X11/extensions/shape.h" HAVE_X11_EXTENSIONS_SHAPE_H )
+  endif( )
+  if( NOT XEXT_FOUND )
+    tde_message_fatal( "You requested Xext support, but was not found on your system." )
+  endif( )
+endif( )
