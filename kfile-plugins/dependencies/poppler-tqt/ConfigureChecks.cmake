03e9329656db37001866d8d225942604f23c8dd9
Timothy Pearson <kb9vqf@pearsoncomputing.net>
2012-09-14 09:32:22 -0500

    Use poppler pkg-config information when running version checks

--- kfile-plugins/dependencies/poppler-tqt/ConfigureChecks.cmake
+++ kfile-plugins/dependencies/poppler-tqt/ConfigureChecks.cmake
@@ -9,8 +9,8 @@ if( BUILD_KPDF )
 endif( )
 
 tde_save( CMAKE_REQUIRED_INCLUDES CMAKE_REQUIRED_LIBRARIES )
-set( CMAKE_REQUIRED_INCLUDES ${DBUS_TQT_INCLUDE_DIRS} ${TQT_INCLUDE_DIRS} ${QT_INCLUDE_DIRS})
-set( CMAKE_REQUIRED_LIBRARIES ${DBUS_TQT_LDFLAGS} ${TQT_LDFLAGS} ${QT_LDFLAGS} "-lpoppler" )
+set( CMAKE_REQUIRED_INCLUDES ${DBUS_TQT_INCLUDE_DIRS} ${TQT_INCLUDE_DIRS} ${QT_INCLUDE_DIRS} ${POPPLER_INCLUDE_DIRS})
+set( CMAKE_REQUIRED_LIBRARIES ${DBUS_TQT_LDFLAGS} ${TQT_LDFLAGS} ${QT_LDFLAGS} ${POPPLER_LDFLAGS} )
 check_cxx_source_compiles("
   #include <poppler/PSOutputDev.h>
   int main(int, char**) { int a; int b; PSOutputDev *psOut = new PSOutputDev(\"test\", (PDFDoc*)0, (XRef*)0, (Catalog*)0, NULL, 0, 0, psModePS, a, b); } "
