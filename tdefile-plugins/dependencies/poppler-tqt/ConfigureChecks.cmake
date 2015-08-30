ac4aa21a129316b9cf2a6785ad9ae1985b199981
Slávek Banko <slavek.banko@axis.cz>
2015-08-30 14:23:37 +0200

    Fix poppler include in configure checks and link library path This resolves Bug 2489
    Signed-off-by: Slávek Banko <slavek.banko@axis.cz>

--- tdefile-plugins/dependencies/poppler-tqt/ConfigureChecks.cmake
+++ tdefile-plugins/dependencies/poppler-tqt/ConfigureChecks.cmake
@@ -10,16 +10,16 @@ tde_save( CMAKE_REQUIRED_INCLUDES CMAKE_REQUIRED_LIBRARIES )
 set( CMAKE_REQUIRED_INCLUDES ${DBUS_TQT_INCLUDE_DIRS} ${TQT_INCLUDE_DIRS} ${QT_INCLUDE_DIRS} ${POPPLER_INCLUDE_DIRS})
 set( CMAKE_REQUIRED_LIBRARIES ${DBUS_TQT_LDFLAGS} ${TQT_LDFLAGS} ${QT_LDFLAGS} ${POPPLER_LDFLAGS} )
 check_cxx_source_compiles("
-  #include <poppler/PSOutputDev.h>
+  #include <PSOutputDev.h>
   int main(int, char**) { int a; int b; PSOutputDev *psOut = new PSOutputDev(\"test\", (PDFDoc*)0, (XRef*)0, (Catalog*)0, NULL, 0, 0, psModePS, a, b); } "
   HAVE_POPPLER_016 )
 check_cxx_source_compiles("
-  #include <poppler/PSOutputDev.h>
+  #include <PSOutputDev.h>
   int main(int, char**) { int a; int b; PSOutputDev *psOut = new PSOutputDev(\"test\", (PDFDoc*)0, NULL, 0, 0, psModePS, a, b); } "
   HAVE_POPPLER_020 )
 check_cxx_source_compiles("
   #include <vector>
-  #include <poppler/PSOutputDev.h>
+  #include <PSOutputDev.h>
   int main(int, char**) { int a; int b; std::vector<int> p; PSOutputDev *psOut = new PSOutputDev(\"test\", (PDFDoc*)0, NULL, p, psModePS, a, b); } "
   HAVE_POPPLER_030 )
 tde_restore( CMAKE_REQUIRED_INCLUDES CMAKE_REQUIRED_LIBRARIES )
