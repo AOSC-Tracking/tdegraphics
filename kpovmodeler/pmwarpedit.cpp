e89dc6d4d091bf144769fdfa3096e12c4fe9af73
Slávek Banko <slavek.banko@axis.cz>
2020-01-01 21:33:50 +0100

    Fix inadvertently renamed Orientation => Qt::Orientation.
    Signed-off-by: Slávek Banko <slavek.banko@axis.cz>

--- kpovmodeler/pmwarpedit.cpp
+++ kpovmodeler/pmwarpedit.cpp
@@ -139,7 +139,7 @@ void PMWarpEdit::createTopWidgets( )
    /* Mapping Warp Objects */
    m_pMappingWidget = new TQWidget( this );
    vl = new TQVBoxLayout( m_pMappingWidget, 0, KDialog::spacingHint( ) );
-   label = new TQLabel( i18n( "Qt::Orientation:" ), m_pMappingWidget );
+   label = new TQLabel( i18n( "Orientation:" ), m_pMappingWidget );
    m_pOrientationEdit = new PMVectorEdit( "x", "y", "z", m_pMappingWidget );
    hl = new TQHBoxLayout( vl );
    hl->addWidget( label );
