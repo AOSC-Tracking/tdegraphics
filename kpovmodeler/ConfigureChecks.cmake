866f96c1f45c0d8302267c3bc3b286369cf9dcfe
Slávek Banko <slavek.banko@axis.cz>
2021-07-08 19:46:45 +0200

    Use FindOpenGL instead of pkg_search_module( GLU ). This resolves FTBFS on FreeBSD 13.
    Signed-off-by: Slávek Banko <slavek.banko@axis.cz>

--- kpovmodeler/ConfigureChecks.cmake
+++ kpovmodeler/ConfigureChecks.cmake
@@ -10,13 +10,14 @@
 #################################################
 
 # glu
-pkg_search_module( GLU glu )
-if( NOT GLU_FOUND )
+set( OpenGL_GL_PREFERENCE LEGACY )
+find_package( OpenGL )
+if( NOT OPENGL_FOUND )
   check_include_file ( GL/glu.h HAVE_GLU )
   if( NOT HAVE_GLU )
-    tde_message_fatal( "glu is required, but was not found on your system" )
+    tde_message_fatal( "OpenGL (glu) is required, but was not found on your system" )
   endif ( )
-  set( GLU_LIBRARIES "-lGL -lGLU" )
+  set( OPENGL_LIBRARIES "-lGL -lGLU" )
 endif( )
 
 
