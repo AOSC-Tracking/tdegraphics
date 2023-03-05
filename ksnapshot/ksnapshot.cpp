5eeaa69b2580f63364ca9f03fae12ca75c493b47
Michele Calgaro <michele.calgaro@yahoo.it>
2023-03-05 23:15:59 +0900

    Revert "KSnapshot: fix Print orientation in ksnapshot cannot be selected. This resolves issue #8." This reverts commit 4ec69a7d0805434abf3897f6d38e4c650fa56582. The original commit is no longer required after commit TDE/tdelibs 9cff750d.

--- ksnapshot/ksnapshot.cpp
+++ ksnapshot/ksnapshot.cpp
@@ -297,6 +297,12 @@ void KSnapshot::slotGrab()
 void KSnapshot::slotPrint()
 {
     KPrinter printer;
+    if (snapshot.width() > snapshot.height())
+        printer.setOrientation(KPrinter::Landscape);
+    else
+        printer.setOrientation(KPrinter::Portrait);
+
+    tqApp->processEvents();
 
     if (printer.setup(this, i18n("Print Screenshot")))
     {
